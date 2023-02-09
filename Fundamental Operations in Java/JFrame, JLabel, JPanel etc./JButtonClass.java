8dca2caf2ba39a7375eac0fe66ac780f2336c99d
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-09 16:11:57 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JButtonClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class JButtonClass {
+    public static void main(String[] args) {
+
+        // JButton = a button that performs an action when clicked on
+
+        new MyFrame();
+    }
+}
