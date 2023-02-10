59b7344e8692157a272df820d9c59340b1861e38
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-10 09:56:34 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JComboBoxPackage/JComboBoxClass.java
@@ -0,0 +1,10 @@
+package com.company.JComboBoxPackage;
+
+public class JComboBoxClass {
+    public static void main(String[] args) {
+
+        // JComboBox = A component that combines a button or editable field and a drop-down list
+
+        new MyFrame();
+    }
+}
