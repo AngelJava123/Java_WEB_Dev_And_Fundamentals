859848e6fe4536f13fe04a052064c72f4362bb56
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-10 09:57:23 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JTextFieldPackage/JTextFieldClass.java
@@ -0,0 +1,10 @@
+package com.company.JTextFieldPackage;
+
+public class JTextFieldClass {
+    public static void main(String[] args) {
+
+        // JTextField = A GUI textbox component that can be used to add, set, ot get text
+
+        new MyFrame();
+    }
+}
