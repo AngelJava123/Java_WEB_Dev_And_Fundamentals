ca2968186407c2f9b240dc13435d18ea4b252609
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-10 09:57:08 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JSliderPackage/Main.java
@@ -0,0 +1,11 @@
+package com.company.JSliderPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JSlider = GUI component that lets user enter a value
+        //           by using an adjustable sliding knob on a track
+
+        SliderClass sliderClass = new SliderClass();
+    }
+}
