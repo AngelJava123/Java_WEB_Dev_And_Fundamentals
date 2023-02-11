c26c5fcb0cde34371b145b5b0af3169bce831136
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-11 03:00:12 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./KeyListenerRocket/Main.java
@@ -0,0 +1,8 @@
+package com.company.KeyListenerRocket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new MyFrame();
+    }
+}
---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./KeyListenerRocket/MyFrame.java
@@ -0,0 +1,62 @@
+package com.company.KeyListenerRocket;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyFrame extends JFrame implements KeyListener {
+
+    JLabel label;
+    ImageIcon icon;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.addKeyListener(this);
+
+        icon = new ImageIcon("C:\\Users\\tasheva\\IdeaProjects\\Additional_Problems\\src\\com\\company\\KeyListener\\rocket.jpg");
+
+        label = new JLabel();
+        label.setBounds(0, 0, 100, 100);
+        label.setIcon(icon);
+        // label.setBackground(Color.red);
+        // label.setOpaque(true);
+
+        this.getContentPane().setBackground(Color.black);
+        this.add(label);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // keyTyped = Invoked when a key is typed. Uses KeyChar, char output
+
+        switch (e.getKeyChar()) {
+            case 'a' -> label.setLocation(label.getX() - 10, label.getY());
+            case 'w' -> label.setLocation(label.getX(), label.getY() - 10);
+            case 's' -> label.setLocation(label.getX(), label.getY() + 10);
+            case 'd' -> label.setLocation(label.getX() + 10, label.getY());
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // keyPressed = Invoked when a physical key is pressed down. Uses KeyCode, int output
+
+        switch (e.getKeyCode()) {
+            case 37 -> label.setLocation(label.getX() - 10, label.getY());
+            case 38 -> label.setLocation(label.getX(), label.getY() - 10);
+            case 39 -> label.setLocation(label.getX() + 10, label.getY());
+            case 40 -> label.setLocation(label.getX(), label.getY() + 10);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // keyReleased = called whenever a button is released
+        // System.out.println("You released key char " + e.getKeyChar());
+        // System.out.println("You released key code " + e.getKeyCode());
+    }
+}
