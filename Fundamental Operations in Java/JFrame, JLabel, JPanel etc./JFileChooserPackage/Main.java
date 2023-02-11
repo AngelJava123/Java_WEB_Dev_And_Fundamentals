8996e621ecc430759d5bf88ab7ff492a299a2416
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-11 02:59:48 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JFileChooserPackage/Main.java
@@ -0,0 +1,10 @@
+package com.company.JFileChooserPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JFileChooser = GUI mechanism that let's user a file (helpful for opening or saving files)
+
+        new MyFrame();
+    }
+}
