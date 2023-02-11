7afe91dcd92ee666ea7156c3890ad891374c3142
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-11 02:59:29 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/JFrame, JLabel, JPanel etc./JColorChooserPackage/Main.java
@@ -0,0 +1,10 @@
+package com.company.JColorChooserPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JColorChooser = A GUI mechanism that let's a user choose a color
+
+        new MyFrame();
+    }
+}
