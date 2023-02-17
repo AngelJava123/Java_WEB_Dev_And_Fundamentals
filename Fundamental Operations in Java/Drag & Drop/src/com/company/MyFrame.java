4bc94e7eec5519ca8c22b4721189ed86647eb17c
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-02-17 23:12:37 +0200

    Add files via upload

---
+++ Fundamental Operations in Java/Drag & Drop/src/com/company/MyFrame.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    DragPanel dragPanel = new DragPanel();
+
+
+    MyFrame() {
+
+        this.add(dragPanel);
+        this.setTitle("Drag & Drop");
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
