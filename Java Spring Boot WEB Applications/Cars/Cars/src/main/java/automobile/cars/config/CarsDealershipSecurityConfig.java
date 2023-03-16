78830a2cc143c04b04b3feadd3d835ca48e07831
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-03-16 19:38:49 +0200

    Update CarsDealershipSecurityConfig.java

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/CarsDealershipSecurityConfig.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/CarsDealershipSecurityConfig.java
@@ -28,7 +28,7 @@ public class CarsDealershipSecurityConfig {
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/", "/login", "/register").permitAll().
-                antMatchers("/contact", "/inquiries", "/terms", "/create").permitAll().
+                antMatchers("/assets/**", "/contact", "/inquiries", "/terms", "/create").permitAll().
                 anyRequest().
                 authenticated().
                 and().formLogin().
