4eefd9f338c696ac671bea73a86c2da09c2a2b8c
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-03-25 00:16:04 +0200

    Update CatalogController.java

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/CatalogController.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/CatalogController.java
@@ -1,13 +1,22 @@
 package automobile.cars.web;
 
+import automobile.cars.service.CatalogService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CatalogController {
 
+    private final CatalogService catalogService;
+
+    public CatalogController(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
     @GetMapping("/catalog")
-    public String catalog() {
+    public String catalog(Model model) {
+        model.addAttribute("allCars", catalogService.getAllCars());
         return "catalog";
     }
 }
