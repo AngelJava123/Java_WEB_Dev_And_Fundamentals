377b923a07cf76bbf93deec0ced10359d55a905b
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-04-02 18:45:43 +0300

    Update FileUploadUtil.java

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/FileUploadUtil.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/FileUploadUtil.java
@@ -1,29 +1,37 @@
 package automobile.cars.config;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.UUID;
 
+@Configuration
 public class FileUploadUtil {
 
-    public static void saveFile(String uploadDir, String fileName,
-                                MultipartFile multipartFile) throws IOException {
-        Path uploadPath = Paths.get(uploadDir);
+    private final Cloudinary cloudinary;
 
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
-        }
+    public FileUploadUtil() {
+        cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "dms1ciius",
+                "api_key", "916444979765684",
+                "api_secret", "HXxr20NSx7tcGERn5Jq27HVNt4k"));
+    }
 
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            Path filePath = uploadPath.resolve(fileName);
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException ioe) {
-            throw new IOException("Could not save image file: " + fileName, ioe);
-        }
+    public String uploadFile(MultipartFile file) throws IOException {
+        // Generate a unique filename
+        String filename = UUID.randomUUID() + "." + StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+        // Upload the file to Cloudinary
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
+                "folder", "AutoGeniusImages",
+                "public_id", filename));
+
+        // Return the public URL of the uploaded file
+        return (String) uploadResult.get("secure_url");
     }
 }
