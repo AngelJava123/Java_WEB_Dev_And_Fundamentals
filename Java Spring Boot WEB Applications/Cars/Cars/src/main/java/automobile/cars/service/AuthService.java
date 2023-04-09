579a6074da75b9a1ba0d1a49fac382b6f579dfc3
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-04-10 01:51:36 +0300

    implement sendEmail method after registration.

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/service/AuthService.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/service/AuthService.java
@@ -21,14 +21,16 @@ public class AuthService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
     private CarsDealershipUserDetailsService carsDealershipUserDetailsService;
 
     public AuthService(UserRepository userRepository,
                        ModelMapper modelMapper,
-                       PasswordEncoder passwordEncoder) {
+                       PasswordEncoder passwordEncoder, EmailService emailService) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
     }
 
     public boolean register(UserRegisterDTO registerDTO) {
@@ -53,6 +55,17 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
 
         userRepository.save(user);
+
+        //Send email after successful registration.
+        String email = user.getEmail();
+        String subject = "Registration Confirmation";
+        String body = "Dear " + user.getUsername() + ",\n\nThank you for registering with AutoGenius!\n\n" +
+                "We're excited to have you on board and can't wait to help you find the car of your dreams.\n\n" +
+                "To get started, log in to your account and browse our wide selection of cars, or search for specific makes and models.\n\n" +
+                "If you have any questions or need assistance, don't hesitate to contact us at support@autogenius.com.\n\n" +
+                "Thank you again for choosing AutoGenius. We look forward to serving you!\n\n" +
+                "Best regards,\nAutoGenius Team";
+        emailService.sendSimpleEmail(email, subject, body);
 
         return true;
     }
