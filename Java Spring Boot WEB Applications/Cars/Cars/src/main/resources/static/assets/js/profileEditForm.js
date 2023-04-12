47ad01f2b139bc22fbf35dd8aa02ebdfb53ac5df
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2023-04-12 16:52:55 +0300

    Update profileEditForm.js

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/resources/static/assets/js/profileEditForm.js
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/resources/static/assets/js/profileEditForm.js
@@ -1,28 +1,30 @@
 function confirmAction() {
-    document.getElementById("popup-container").style.display = "block";
-    return false;
-  }
+  document.getElementById("popup-container").style.display = "block";
+  return false;
+}
 
-  function hidePopup() {
-    document.getElementById("popup-container").style.display = "none";
-    document.getElementById("filedCheck-popup-container").style.display = "none";
-  }
+function hidePopup() {
+  document.getElementById("popup-container").style.display = "none";
+  document.getElementById("Email").classList.remove("is-invalid");
+  document.getElementById("email-error").style.display = "none";
+}
 
-  function validateEmail(email) {
+function validateEmail(email) {
   const re = /\S+@\S+.\S+/;
   return re.test(email);
-  }
+}
 
-  function submitForm() {
-
+function submitForm() {
   let emailField = document.getElementById("Email").value;
 
   if (validateEmail(emailField)) {
     document.getElementById("popup-container").style.display = "none";
-    document.getElementById("filedCheck-popup-container").style.display = "none";
+    document.getElementById("Email").classList.remove("is-invalid");
+    document.getElementById("email-error").style.display = "none";
     document.getElementById("edit-form").submit();
   } else {
     document.getElementById("popup-container").style.display = "none";
-    document.getElementById("filedCheck-popup-container").style.display = "block";
+    document.getElementById("Email").classList.add("is-invalid");
+    document.getElementById("email-error").style.display = "block";
   }
 }
