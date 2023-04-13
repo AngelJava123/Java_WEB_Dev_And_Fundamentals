ed233a53e09546139cad97aa3b20da5620c0a74c
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-13 23:25:11 +0300

    Update UserController.java

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/UserController.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/UserController.java
@@ -1,25 +1,15 @@
 package automobile.cars.web;
 
-import automobile.cars.model.user.CarsDealershipUserDetails;
-import automobile.cars.service.AuthService;
-import automobile.cars.view.ProfileViewModel;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @Controller
 public class UserController {
-
-    private final AuthService authService;
-
-    public UserController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @GetMapping("/login")
     public String login() {
@@ -36,22 +26,4 @@ public class UserController {
 
         return "redirect:/login";
     }
-
-    @GetMapping("/profile")
-    public String profile(Model model,
-                          @AuthenticationPrincipal CarsDealershipUserDetails userDetails) {
-
-        model.addAttribute("profileViewModel", new ProfileViewModel());
-        model.addAttribute("profileDetails", authService.getUserDetails(userDetails));
-
-        return "profile";
-    }
-
-    @PostMapping("/profile")
-    public String updateProfile(@ModelAttribute("profileViewModel") ProfileViewModel profileViewModel,
-                                @AuthenticationPrincipal CarsDealershipUserDetails userDetails) {
-        authService.updateUserDetails(userDetails, profileViewModel);
-        return "redirect:/profile";
-    }
-
 }
