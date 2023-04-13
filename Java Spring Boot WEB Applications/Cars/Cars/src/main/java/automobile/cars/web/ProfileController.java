a92e4ca27db6e867712ac0ca461adef0034dc743
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-13 23:25:54 +0300

    Update and rename ProfileSettingsController.java to ProfileController.java

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/ProfileController.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/ProfileController.java
@@ -1,11 +1,12 @@
 package automobile.cars.web;
 
 import automobile.cars.model.dto.ChangeEmailDTO;
-import automobile.cars.model.dto.InquiryDTO;
 import automobile.cars.model.user.CarsDealershipUserDetails;
-import automobile.cars.service.ProfileSettingsService;
+import automobile.cars.service.ProfileService;
+import automobile.cars.view.ProfileViewModel;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,12 +17,22 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
-public class ProfileSettingsController {
+public class ProfileController {
 
-    private final ProfileSettingsService profileSettingsService;
+    private final ProfileService profileService;
 
-    public ProfileSettingsController(ProfileSettingsService profileSettingsService) {
-        this.profileSettingsService = profileSettingsService;
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model,
+                          @AuthenticationPrincipal CarsDealershipUserDetails userDetails) {
+
+        model.addAttribute("profileViewModel", new ProfileViewModel());
+        model.addAttribute("profileDetails", profileService.getUserDetails(userDetails));
+
+        return "profile";
     }
 
     @GetMapping("/change-email")
@@ -46,7 +57,7 @@ public class ProfileSettingsController {
                               HttpSession session,
                               RedirectAttributes redirectAttributes) {
 
-        if (bindingResult.hasErrors() || !this.profileSettingsService.changeEmail(userDetails, changeEmailDTO)) {
+        if (bindingResult.hasErrors() || !this.profileService.changeEmail(userDetails, changeEmailDTO)) {
             redirectAttributes.addFlashAttribute("changeEmailDTO", changeEmailDTO);
             redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.changeEmailDTO", bindingResult);
 
