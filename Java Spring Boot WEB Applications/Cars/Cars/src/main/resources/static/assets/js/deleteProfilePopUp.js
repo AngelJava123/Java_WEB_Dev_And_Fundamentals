df5eb190620f0f8b298097c884494e45e0c2c1bb
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-17 02:31:35 +0300

    Add files via upload

---
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/resources/static/assets/js/deleteProfilePopUp.js
@@ -0,0 +1,11 @@
+function showPopup() {
+        document.getElementById("popup-container").style.display = "block";
+    }
+
+    function hidePopup() {
+        document.getElementById("popup-container").style.display = "none";
+    }
+
+    function submitForm() {
+        document.getElementById("delete-account-form").submit();
+    }
