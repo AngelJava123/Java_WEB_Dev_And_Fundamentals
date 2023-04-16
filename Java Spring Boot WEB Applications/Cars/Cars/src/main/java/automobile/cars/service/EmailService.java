c7b7e85a0ad93b96f0f0ca11697e89276fc91211
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-16 03:40:46 +0300

    changed email service to work with html files

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/service/EmailService.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/service/EmailService.java
@@ -3,7 +3,11 @@ package automobile.cars.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -14,14 +18,19 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
-    public void sendSimpleEmail(String toEmail,
-                                String subject,
-                                String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("autogenius.app@gmail.com");
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
+    public void sendHtmlEmail(String toEmail,
+                              String subject,
+                              String body) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom("autogenius.app@gmail.com");
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        helper.setText(body, true);
+
         mailSender.send(message);
     }
+
 }
