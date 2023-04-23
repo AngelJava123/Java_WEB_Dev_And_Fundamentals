c0cd2c1c2d90b36bd8c00a44cb009dd0f9b38a60
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-24 00:02:38 +0300

    Adding favourites

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/CatalogController.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/CatalogController.java
@@ -1,12 +1,15 @@
 package automobile.cars.web;
 
+import automobile.cars.model.user.CarsDealershipUserDetails;
 import automobile.cars.service.CatalogService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CatalogController {
@@ -26,5 +29,34 @@ public class CatalogController {
                                   size = 5) Pageable pageable) {
         model.addAttribute("allCars", catalogService.getAllCars(pageable));
         return "catalog";
+    }
+
+    @GetMapping("/add-favourite/{id}")
+    public String add(@PathVariable("id") Long id,
+                      @AuthenticationPrincipal CarsDealershipUserDetails user) {
+
+        this.catalogService.addCarToUserFavourites(id, user);
+
+        return "redirect:/catalog";
+    }
+
+    @GetMapping("/remove-favourite/{id}")
+    public String remove(@PathVariable("id") Long id,
+                      @AuthenticationPrincipal CarsDealershipUserDetails user) {
+
+        this.catalogService.removeCarFromUserFavourites(id, user);
+
+        return "redirect:/catalog";
+    }
+
+    @GetMapping("/favourites")
+    public String favourites(Model model,
+                          @PageableDefault(
+                                  sort = "make",
+                                  direction = Sort.Direction.ASC,
+                                  page = 0,
+                                  size = 5) Pageable pageable) {
+        model.addAttribute("allCars", catalogService.getFavouriteCars(pageable));
+        return "favourites";
     }
 }
