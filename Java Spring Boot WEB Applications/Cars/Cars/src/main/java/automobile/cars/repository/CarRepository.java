7e4509a9b0f3bf5547bf83c3d89b349cc0d71328
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-27 22:40:47 +0300

    Search for cars native query initialized

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/repository/CarRepository.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/repository/CarRepository.java
@@ -1,15 +1,32 @@
 package automobile.cars.repository;
 
 import automobile.cars.model.entity.Car;
+import automobile.cars.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-  
-  Page<Car> findByUser(User user, Pageable pageable);
-  
-  Optional<Car> findByMileage(String mileage);
-  
-  Page<Car> findByFavouriteTrue(Pageable pageable);
+
+    Page<Car> findByUser(User user, Pageable pageable);
+
+    Optional<Car> findByMileage(String mileage);
+
+    Page<Car> findByFavouriteTrue(Pageable pageable);
+
+    Page<Car> findAllByMakeAndModel(String make, String model, Pageable pageable);
+
+    @Query(value = "SELECT * FROM cars WHERE make = COALESCE(:make, make) AND model = COALESCE(:model, model) AND year = COALESCE(:year, year)", nativeQuery = true)
+    Page<Car> searchCars(@Param("make") String make,
+                         @Param("model") String model,
+                         @Param("year") String year,
+                         Pageable pageable);
+
+
 }
