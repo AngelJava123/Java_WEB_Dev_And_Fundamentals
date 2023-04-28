5183bb6bf8e9aa9553a4c82b9bc6e52d0c058b09
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-28 23:30:41 +0300

    Added findAll by spec and pageable

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/repository/CarRepository.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/repository/CarRepository.java
@@ -4,9 +4,8 @@ import automobile.cars.model.entity.Car;
 import automobile.cars.model.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -20,13 +19,6 @@ public interface CarRepository extends JpaRepository<Car, Long> {
 
     Page<Car> findByFavouriteTrue(Pageable pageable);
 
-    Page<Car> findAllByMakeAndModel(String make, String model, Pageable pageable);
-
-    @Query(value = "SELECT * FROM cars WHERE make = COALESCE(:make, make) AND model = COALESCE(:model, model) AND year = COALESCE(:year, year)", nativeQuery = true)
-    Page<Car> searchCars(@Param("make") String make,
-                         @Param("model") String model,
-                         @Param("year") String year,
-                         Pageable pageable);
-
+    Page<Car> findAll(Specification<Car> spec, Pageable pageable);
 
 }
