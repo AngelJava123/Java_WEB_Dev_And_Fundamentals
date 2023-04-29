ee8922033f71e86efc8b50aeaae0c0a3e66c749a
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-04-30 01:57:19 +0300

    Added some fields

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/CarSpecification.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/CarSpecification.java
@@ -1,7 +1,11 @@
 package automobile.cars.config;
 
-import automobile.cars.model.entity.Car;
+import automobile.cars.model.entity.*;
 import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.JoinType;
+
+import javax.persistence.criteria.Join;
 
 public interface CarSpecification {
     static Specification<Car> make(String make) {
@@ -37,6 +41,85 @@ public interface CarSpecification {
                 return null;
             }
             return builder.equal(root.get("year"), year);
+        };
+    }
+
+    static Specification<Car> engineType(String engineType) {
+        return (root, query, builder) -> {
+            if (engineType == null || engineType.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleEngine> engineJoin = root.join("engine", JoinType.LEFT);
+            return builder.equal(engineJoin.get("engineType"), engineType);
+        };
+    }
+
+    static Specification<Car> enginePower(String enginePower) {
+        return (root, query, builder) -> {
+            if (enginePower == null || enginePower.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleEngine> engineJoin = root.join("engine", JoinType.LEFT);
+            return builder.equal(engineJoin.get("power"), enginePower);
+        };
+    }
+
+    static Specification<Car> engineEuroStandard(String engineEuroStandard) {
+        return (root, query, builder) -> {
+            if (engineEuroStandard == null || engineEuroStandard.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleEngine> engineJoin = root.join("engine", JoinType.LEFT);
+            return builder.equal(engineJoin.get("euroStandard"), engineEuroStandard);
+        };
+    }
+
+    static Specification<Car> engineCubicCapacity(String engineCubicCapacity) {
+        return (root, query, builder) -> {
+            if (engineCubicCapacity == null || engineCubicCapacity.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleEngine> engineJoin = root.join("engine", JoinType.LEFT);
+            return builder.equal(engineJoin.get("cubicCapacity"), engineCubicCapacity);
+        };
+    }
+
+    static Specification<Car> category(String category) {
+        return (root, query, builder) -> {
+            if (category == null || category.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleCategory> engineJoin = root.join("category", JoinType.LEFT);
+            return builder.equal(engineJoin.get("category"), category);
+        };
+    }
+
+    static Specification<Car> gearBox(String gearBox) {
+        return (root, query, builder) -> {
+            if (gearBox == null || gearBox.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleGearBox> engineJoin = root.join("gearBox", JoinType.LEFT);
+            return builder.equal(engineJoin.get("gearBoxType"), gearBox);
+        };
+    }
+
+    static Specification<Car> mileage(String mileage) {
+        return (root, query, builder) -> {
+            if (mileage == null || mileage.isEmpty()) {
+                return null;
+            }
+            return builder.equal(root.get("mileage"), mileage);
+        };
+    }
+
+    static Specification<Car> color(String color) {
+        return (root, query, builder) -> {
+            if (color == null || color.isEmpty()) {
+                return null;
+            }
+            Join<Car, VehicleColor> engineJoin = root.join("color", JoinType.LEFT);
+            return builder.equal(engineJoin.get("paint"), color);
         };
     }
 
