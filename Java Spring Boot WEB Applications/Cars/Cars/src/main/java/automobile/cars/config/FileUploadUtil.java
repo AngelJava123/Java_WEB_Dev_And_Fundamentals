f1d9de279261c8b85b15297c692e2bf75c8bbd75
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-05-04 15:52:36 +0300

    added method delete images when remove Car

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/FileUploadUtil.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/config/FileUploadUtil.java
@@ -2,13 +2,17 @@ package automobile.cars.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Configuration
 public class FileUploadUtil {
@@ -17,12 +21,10 @@ public class FileUploadUtil {
 
     public FileUploadUtil() {
         cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "my_cloud_name",
-                "api_key", "my_api_key",
-                "api_secret", "my_api_secret"));
+                "cloud_name", "dms1ciius",
+                "api_key", "916444979765684",
+                "api_secret", "HXxr20NSx7tcGERn5Jq27HVNt4k"));
     }
-    
-    // I will give the cloudinary details for access on demand, since it is a sensitive information and should not be public.
 
     public String uploadFile(MultipartFile file) throws IOException {
         // Generate a unique filename
@@ -30,10 +32,24 @@ public class FileUploadUtil {
 
         // Upload the file to Cloudinary
         Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
-                "folder", "my_folder_name",
+                "folder", "AutoGeniusImages",
                 "public_id", filename));
 
         // Return the public URL of the uploaded file
         return (String) uploadResult.get("secure_url");
     }
+
+    public void deleteFiles(List<String> imageFilePaths) throws IOException {
+        for (String path : imageFilePaths) {
+            // Extract the public ID from the image URL
+            Pattern pattern = Pattern.compile("upload/v\\d+/(.*?)(\\.[^.]*$|$)");
+            Matcher matcher = pattern.matcher(path);
+            if (matcher.find()) {
+                String publicId = matcher.group(1);
+                // Delete the file from Cloudinary
+                cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+            }
+        }
+    }
+
 }
