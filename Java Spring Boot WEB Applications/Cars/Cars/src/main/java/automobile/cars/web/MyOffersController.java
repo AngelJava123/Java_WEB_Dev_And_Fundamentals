f3e5a1d5ffb4c7f6e66d1b87c6284c9fc8e9a2a0
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2023-05-07 00:57:07 +0300

    added @GetMapping("/edit-img/{id}")

--- Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/MyOffersController.java
+++ Java Spring Boot WEB Applications/Cars/Cars/src/main/java/automobile/cars/web/MyOffersController.java
@@ -3,6 +3,7 @@ package automobile.cars.web;
 import automobile.cars.model.dto.CreateCarDTO;
 import automobile.cars.model.entity.Car;
 import automobile.cars.model.user.CarsDealershipUserDetails;
+import automobile.cars.service.CatalogService;
 import automobile.cars.service.MyOffersService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -24,9 +26,11 @@ public class MyOffersController {
 
     private final MyOffersService myOffersService;
     public static Long editCarId;
+    private final CatalogService catalogService;
 
-    public MyOffersController(MyOffersService myOffersService) {
+    public MyOffersController(MyOffersService myOffersService, CatalogService catalogService) {
         this.myOffersService = myOffersService;
+        this.catalogService = catalogService;
     }
 
     @GetMapping("/myOffers")
@@ -43,7 +47,9 @@ public class MyOffersController {
     }
 
     @GetMapping("/remove/{id}")
-    public String removeCar(@PathVariable("id") Long id) {
+    public String removeCar(@PathVariable("id") Long id,
+                            @AuthenticationPrincipal CarsDealershipUserDetails userDetails) throws IOException {
+        catalogService.removeCarFromUserFavourites(id, userDetails);
         myOffersService.deleteById(id);
         return "redirect:/myOffers";
     }
@@ -61,6 +67,15 @@ public class MyOffersController {
     public String saveEdit(@ModelAttribute("car") CreateCarDTO carDTO) throws IOException {
         myOffersService.updateCar(carDTO, editCarId);
         return "redirect:/myOffers";
+    }
+
+    @GetMapping("/edit-img/{id}")
+    public ModelAndView editImage(@PathVariable("id") Long id) {
+        Optional<Car> car = myOffersService.getCarById(id);
+        ModelAndView modelAndView = new ModelAndView("edit-image");
+        modelAndView.addObject("car", car.get());
+        editCarId = id;
+        return modelAndView;
     }
 
 }
